13c60a7fd5f47c48fdf40b6d7eaeb4d7e64be10b
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-01-17 19:28:20 +0530

    separate card data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,9 @@
-import CardImage from "../assets/katie-zaferes.png"
-import Star from "../assets/star.png"
+import CardData from "./CardData"
 
 export default function Card(){
     return(
         <div className="container-card">
-            <div className="card">
-                <img className="card-img" src={CardImage} alt="" />
-                <div className="card-star">
-                    <img src={Star} alt="star" />
-                    <span>5.0</span>
-                    <span className="gray">(6) •</span>
-                    <span className="gray">USA</span>
-                </div>
-                <p>Life Lessons with Katie Zaferes</p>
-                <p><span className="bold">From $136</span> / Perosn</p>
-            </div>
+            <CardData />
         </div>
     )
 }
---
+++ src/components/CardData.jsx
@@ -0,0 +1,18 @@
+import CardImage from "../assets/katie-zaferes.png"
+import Star from "../assets/star.png"
+
+export default function CardData(){
+    return(
+        <div className="card">
+                <img className="card-img" src={CardImage} alt="" />
+                <div className="card-star">
+                    <img src={Star} alt="star" />
+                    <span>5.0</span>
+                    <span className="gray">(6) •</span>
+                    <span className="gray">USA</span>
+                </div>
+                <p>Life Lessons with Katie Zaferes</p>
+                <p><span className="bold">From $136</span> / Perosn</p>
+        </div>
+    )
+}
