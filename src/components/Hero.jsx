e197df79008d91619e23aa80b88935b181569e50
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-01-17 12:13:05 +0530

    font and hero image styling

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import PhotoGrid from "../assets/photo-grid.png"
+export default function Hero(){
+    return(
+        <div className="hero">
+            <img src={PhotoGrid} className="hero-img" alt="Photo Grid" />
+            <h1 className="hero-header">Online Experiences</h1>
+            <p className="hero-text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+    )
+}
