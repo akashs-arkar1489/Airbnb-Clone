89831bb6c8292ce1697e0255685f7760cebeeb9f
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-01-22 20:04:40 +0530

    passing as objects

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,14 +3,9 @@ import Data from "./card-data/Data"
 
 export default function Card(){
     const viewdata = Data.map((each_data) => {
-        return <CardData key={each_data.id}
-            title={each_data.title}
-            imgurl={each_data.coverImg}
-            rating={each_data.stats.rating}
-            reviewCount={each_data.stats.reviewCount}
-            country={each_data.location}
-            price={each_data.price}
-            status={each_data.openSpots}
+        return <CardData 
+            key={each_data.id}
+            item = {each_data}
         />
     })
     return(
--- src/components/card-data/CardData.jsx
+++ src/components/card-data/CardData.jsx
@@ -2,23 +2,23 @@ import Star from "/images/star.png"
 
 export default function CardData(props){
     let badgeText;
-    if (props.status === 0){
+    if (props.item.openSpots === 0){
         badgeText = "Sold Out"
-    }else if (props.country === "Online" ){
+    }else if (props.item.location === "Online" ){
         badgeText = "ONLINE"
     }
     return(
         <div className="card">
             {badgeText && <div className={badgeText === "ONLINE" ? "card-badge-online" : "card-badge"}><span style={badgeText === "ONLINE" ? {display: "block"}: {display: "none"}} className="blink"></span> {badgeText}</div> }
-            <img className="card-img" src={props.imgurl} alt={props.imgurl} />
+            <img className="card-img" src={props.item.coverImg} alt={props.item.coverImg} />
             <div className="card-star">
                 <img src={Star} alt="star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) •</span>
-                <span className="gray">{props.country}</span>
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) •</span>
+                <span className="gray">{props.item.location}</span>
             </div>
-            <p>{props.title}</p>
-            <p className="fixd-text"><span className="bold">From ${props.price}</span> / Perosn</p>
+            <p>{props.item.title}</p>
+            <p className="fixd-text"><span className="bold">From ${props.item.price}</span> / Perosn</p>
         </div>
     )
 }
